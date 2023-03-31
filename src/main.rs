a3f2c7817dae1087c262303335e327bf7ee30e7f
Bryce Berger <bryce.z.berger@gmail.com>
2023-03-31 04:38:56 -0400

    Switch from actix-web to axum

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ mod package;
 mod queries;
 mod user;
 
-use actix_cors::Cors;
-use actix_web::{http, App, HttpServer};
+use queries::*;
 
+use axum::{
+    http::{header, HeaderValue, Method},
+    routing::{get, post, put},
+    Router,
+};
 use firestore::FirestoreDb;
+use http::HeaderName;
 use once_cell::sync::Lazy;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use tower_http::cors::CorsLayer;
 
 pub static DB: Lazy<FirestoreDb> =
     Lazy::new(|| futures::executor::block_on(async { init_database().await }));
@@ -17,36 +24,39 @@ async fn init_database() -> FirestoreDb {
     FirestoreDb::new("ece-461-dev").await.unwrap()
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        let cors = Cors::default()
-            .allowed_origin("https://web.gcp.sammelson.com")
-            .allowed_methods(vec![
-                http::Method::DELETE,
-                http::Method::GET,
-                http::Method::POST,
-                http::Method::PUT,
-            ])
-            .allowed_header("X-Authorization")
-            .allowed_header("offset")
-            .max_age(3600);
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/package", post(queries::post_package))
+        .route(
+            "/package/:id",
+            get(get_package_by_id)
+                .put(update_package_by_id)
+                .delete(delete_package_by_id),
+        )
+        .route("/packages", post(search_packages))
+        .route("/package/{id}/:rate", get(get_rating_by_id))
+        .route("/authenticate", put(authenticate))
+        .route(
+            "/package/byName/:name",
+            get(get_package_by_name).delete(delete_package_by_name),
+        )
+        .route("/package/byRegEx", get(get_package_by_regex))
+        .layer(
+            CorsLayer::new()
+                .allow_origin("http://localhost:8000".parse::<HeaderValue>().unwrap())
+                .allow_headers([header::CONTENT_TYPE, HeaderName::from_static("offset")])
+                .allow_methods([Method::GET, Method::POST, Method::DELETE, Method::PUT]),
+        );
 
-        App::new()
-            .service(queries::authenticate)
-            .service(queries::post_package)
-            .service(queries::reset_registry)
-            .service(queries::search_packages)
-            .service(queries::get_rating_by_id)
-            .service(queries::get_package_by_id)
-            .service(queries::get_package_by_name)
-            .service(queries::update_package_by_id)
-            .service(queries::delete_package_by_id)
-            .service(queries::get_package_by_regex)
-            .service(queries::delete_package_by_name)
-            .wrap(cors)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)),
+        8080,
+    ))
+    .serve(app.into_make_service())
+    .await?;
+
+    Ok(())
 }
--- src/queries.rs
+++ src/queries.rs
@@ -2,78 +2,90 @@ use crate::package::*;
 use crate::user::*;
 use crate::{DB, METADATA};
 
-use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
+use axum::{
+    extract::{Json, Path},
+    response::IntoResponse,
+};
 use firestore::*;
 use futures::{future, stream::BoxStream, StreamExt};
+use http::{header, StatusCode};
+
+/// helper function because can't do
+/// `impl<T> IntoResponse for T where T: Serialize`
+fn serialize(data: impl serde::Serialize) -> impl IntoResponse {
+    serde_json::to_string(&data).unwrap()
+}
+
+/// helper function for constructing common use case of returning status ok with json body
+fn ok(data: impl serde::Serialize) -> impl IntoResponse {
+    respond(StatusCode::OK, data)
+}
+
+fn respond(code: StatusCode, data: impl serde::Serialize) -> impl IntoResponse {
+    (
+        code,
+        [(header::CONTENT_TYPE, "application/json")],
+        serialize(data),
+    )
+}
 
 /// Get the packages from the registry.
 ///
 /// Get any packages fitting the query. Search for packages satisfying the indicated query.
 /// If you want to enumerate all packages, provide an array with a single PackageQuery whose name is "*".
 /// The response is paginated; the response header includes the offset to use in the next query.
-#[post("/packages")]
-pub async fn search_packages(search: web::Json<Vec<SearchQuery>>) -> impl Responder {
+pub async fn search_packages(Json(search): Json<Vec<SearchQuery>>) -> impl IntoResponse {
     // 200: list of packages
     // 413: too many packages returned (shouldn't happen? it's paginated)
-    let search = search.into_inner();
-    HttpResponse::NotImplemented().json(
-        search
-            .iter()
-            .map(|_| PackageMetadata::default())
-            .collect::<Vec<_>>(),
-    )
+    ok(search
+        .iter()
+        .map(|_| PackageMetadata::default())
+        .collect::<Vec<_>>())
 }
 
 /// Reset the registry
 ///
 /// Reset the registry to a system default state.
-#[delete("/reset")]
-pub async fn reset_registry() -> impl Responder {
+pub async fn _reset_registry() -> impl IntoResponse {
     // 200: reset registry
     // 401: not authorized
-    HttpResponse::NotImplemented().finish()
+    StatusCode::NOT_IMPLEMENTED
 }
 
 /// Interact with the package with this ID
 ///
 /// Return this package.
-#[get("/package/{id}")]
-pub async fn get_package_by_id(_id: web::Path<PackageId>) -> impl Responder {
+pub async fn get_package_by_id(Path(_id): Path<PackageId>) -> impl IntoResponse {
     // 200: return package
     // 404: does not exist
-    HttpResponse::NotImplemented().json(Package::default())
+    StatusCode::NOT_IMPLEMENTED
 }
 
 /// Update the content of the package.
 ///
 /// The name, version, and ID must match.
 /// The package contents (from PackageData) will replace the previous contents.
-#[put("/package/{id}")]
 pub async fn update_package_by_id(
-    _id: web::Path<PackageId>,
-    _info: web::Json<Package>,
-) -> impl Responder {
+    Path(_id): Path<PackageId>,
+    Json(_info): Json<Package>,
+) -> impl IntoResponse {
     // 200: package updated
     // 404: does not exist
-    HttpResponse::NotImplemented().finish()
+    StatusCode::NOT_IMPLEMENTED
 }
 
 /// Delete this version of the package.
-#[delete("/package/{id}")]
-pub async fn delete_package_by_id(_id: web::Path<PackageId>) -> impl Responder {
+pub async fn delete_package_by_id(Path(_id): Path<PackageId>) -> impl IntoResponse {
     // 200: package deleted
     // 404: does not exist
-    HttpResponse::NotImplemented().finish()
+    StatusCode::NOT_IMPLEMENTED
 }
 
-#[post("/package")]
-pub async fn post_package(to_upload: web::Json<Package>) -> impl Responder {
+pub async fn post_package(Json(to_upload): Json<Package>) -> Result<impl IntoResponse, StatusCode> {
     // not yet implemeted:
     // 403: auth failed
     // 424: failed due to bad rating
-    let to_upload = to_upload.into_inner();
-
-    let prev_versions: FirestoreResult<BoxStream<PackageMetadata>> = DB
+    let prev_versions: BoxStream<PackageMetadata> = DB
         .fluent()
         .select()
         .from(METADATA)
@@ -83,13 +95,8 @@ pub async fn post_package(to_upload: web::Json<Package>) -> impl Responder {
         })
         .obj()
         .stream_query()
-        .await;
-
-    let prev_versions = match prev_versions {
-        Ok(p) => p,
-        // 500: ??? some database error
-        Err(e) => return HttpResponse::InternalServerError().body(e.to_string()),
-    };
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     if prev_versions
         .filter(|m| future::ready(m.version == to_upload.metadata.version))
@@ -98,58 +105,56 @@ pub async fn post_package(to_upload: web::Json<Package>) -> impl Responder {
         >= 1
     {
         // 409: package already exists
-        return HttpResponse::Conflict().finish();
+        return Err(StatusCode::CONFLICT);
     }
 
     let id = PackageId::new();
-    let response: FirestoreResult<PackageMetadata> = DB
+    let _: PackageMetadata = DB
         .fluent()
         .insert()
         .into(METADATA)
         .document_id(id.to_string())
         .object(&to_upload.metadata)
         .execute()
-        .await;
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    match response {
-        // 201: return package, with correct ID
-        Ok(PackageMetadata { id, .. }) => HttpResponse::Created().json(Package {
+    // 201: return package, with correct ID
+    Ok(respond(
+        StatusCode::CREATED,
+        Package {
             metadata: PackageMetadata {
                 id,
                 ..to_upload.metadata
             },
             ..to_upload
-        }),
-        // 500: ??? some database error
-        Err(e) => HttpResponse::InternalServerError().body(e.to_string()),
-    }
+        },
+    ))
 }
 
-#[get("/package/{id}/rate")]
-pub async fn get_rating_by_id(_id: web::Path<PackageId>) -> impl Responder {
+// TODO
+pub async fn get_rating_by_id(Path(_id): Path<PackageId>) -> impl IntoResponse {
     // 200: return rating iff all rated
     // 404: does not exist
     // 500: package rating error
-    HttpResponse::NotImplemented().json(PackageRating::default())
+    ok(PackageRating::default())
 }
 
 /// Create an access token.
-#[put("/authenticate")]
-pub async fn authenticate(auth: web::Json<AuthenticationRequest>) -> impl Responder {
+// TODO
+pub async fn authenticate(Json(auth): Json<AuthenticationRequest>) -> impl IntoResponse {
     // 200: return token
     // 401: invalid user/password
     // 501: not implemented
-    let auth = auth.into_inner();
-    HttpResponse::NotImplemented().json(AuthenticationToken::new(auth))
+    ok(AuthenticationToken::new(auth))
 }
 
 /// Return the history of this package (all versions).
-#[get("/package/byName/{name}")]
-pub async fn get_package_by_name(name: web::Path<String>) -> impl Responder {
+// TODO
+pub async fn get_package_by_name(Path(name): Path<String>) -> impl IntoResponse {
     // 200: return package history
     // 404: does not exist
-    let name = name.into_inner();
-    HttpResponse::NotImplemented().json(vec![PackageHistoryEntry {
+    ok(vec![PackageHistoryEntry {
         metadata: PackageMetadata {
             name,
             ..Default::default()
@@ -159,19 +164,19 @@ pub async fn get_package_by_name(name: web::Path<String>) -> impl Responder {
 }
 
 /// Delete all versions of this package.
-#[delete("/package/byName/{name}")]
-pub async fn delete_package_by_name(_name: web::Path<String>) -> impl Responder {
+// TODO
+pub async fn delete_package_by_name(Path(_name): Path<String>) -> impl IntoResponse {
     // 200: package deleted
     // 404: does not exist
-    HttpResponse::NotImplemented().finish()
+    StatusCode::NOT_IMPLEMENTED
 }
 
 /// Get any packages fitting the regular expression.
 ///
 /// Search for a package using regular expression over package names and READMEs.
-#[get("/package/byRegEx")]
-pub async fn get_package_by_regex(_regex: web::Json<String>) -> impl Responder {
+// TODO
+pub async fn get_package_by_regex(_regex: String) -> impl IntoResponse {
     // 200: return list of packages
     // 404: no packages found
-    HttpResponse::NotImplemented().json(vec![PackageMetadata::default()])
+    ok(vec![PackageMetadata::default()])
 }
