e09a02948197350824eec9e91513ca1c7ba6da49
Bryce Berger <bryce.z.berger@gmail.com>
2023-04-14 17:35:45 -0400

    Reorganization
    Split endpoints off into their own module

---
+++ src/queries/endpoints/mod.rs
@@ -0,0 +1,264 @@
+#[cfg(test)]
+mod tests;
+
+use super::*;
+use crate::{package::*, user::*};
+
+use axum::{
+    extract::{Json, Path, Query},
+    http::{HeaderName, HeaderValue, StatusCode},
+    response::IntoResponse,
+};
+use firestore::{FirestoreQueryCursor, FirestoreQueryDirection, FirestoreValue};
+use semver::VersionReq;
+
+#[derive(serde::Deserialize)]
+pub struct Offset {
+    offset: Option<String>,
+}
+
+impl Offset {
+    fn parse(offset: &str) -> Option<Vec<FirestoreValue>> {
+        offset
+            .split_once(',')
+            .map(|(v, i)| vec![v.into(), i.into()])
+    }
+}
+
+/// Get the packages from the registry.
+///
+/// Get any packages fitting the query. Search for packages satisfying the indicated query.
+/// If you want to enumerate all packages, provide an array with a single PackageQuery whose name is "*".
+/// The response is paginated; the response header includes the offset to use in the next query.
+pub async fn search_packages(
+    Query(Offset { offset }): Query<Offset>,
+    Json(search): Json<Vec<SearchQuery>>,
+) -> Result<MyResponse<Vec<PackageMetadata>>, StatusCode> {
+    // have to have packages to search for
+    if search.len() < 1 {
+        return Err(StatusCode::BAD_REQUEST);
+    }
+
+    // don't want to support this
+    if search.len() > 1 {
+        return Err(StatusCode::REQUEST_HEADER_FIELDS_TOO_LARGE);
+    }
+
+    // know at least one element from early return above
+    let search = search.into_iter().next().unwrap();
+
+    let db = constants::get_database().await;
+
+    let start = offset.and_then(|offs| Offset::parse(&offs));
+    let show_all = search.name == "*";
+
+    let requires_eq = search
+        .version
+        .as_ref()
+        .map(|v| {
+            v.comparators
+                .iter()
+                .find_map(|c| filter::comparator_requires_eq(c).then_some(c.clone()))
+        })
+        .flatten();
+    let one_sort = requires_eq.is_some();
+    let version = match requires_eq {
+        Some(requires_eq) => Some(VersionReq {
+            comparators: vec![requires_eq],
+        }),
+        None => search.version,
+    };
+
+    let query = db
+        .fluent()
+        .select()
+        .from(constants::METADATA)
+        .limit(constants::PAGE_LIMIT as u32);
+
+    // filter out packages with names that don't match
+    let query = if show_all {
+        query.filter(|q| q.for_all(filter::versionreq_to_filter(&q, version.as_ref()?)))
+    } else {
+        query.filter(|q| {
+            q.for_all([
+                q.field("Name").eq(&search.name),
+                version
+                    .as_ref()
+                    .and_then(|version| filter::versionreq_to_filter(&q, version)),
+            ])
+        })
+    };
+
+    let query = if one_sort {
+        query.order_by([("ID", FirestoreQueryDirection::Ascending)])
+    } else {
+        query.order_by([
+            ("Version", FirestoreQueryDirection::Ascending),
+            ("ID", FirestoreQueryDirection::Ascending),
+        ])
+    };
+
+    // start at the offset given
+    let query = match start {
+        Some(start) => query.start_at(FirestoreQueryCursor::AfterValue(if one_sort {
+            start.into_iter().skip(1).collect()
+        } else {
+            start
+        })),
+        None => query,
+    };
+
+    // get a set of packages that have names that match
+    let result: Vec<PackageMetadata> = query.obj().query().await.map_err(|e| {
+        log::error!("{}", e);
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
+
+    // 200: list of packages
+    Ok(if result.len() < constants::PAGE_LIMIT {
+        // this is the last page, don't provide an offset for the next query
+        ok(result)
+    } else {
+        let last_id = result
+            .last()
+            .map(|last| {
+                HeaderValue::from_str(&format!("{},{}", last.version, last.id.as_ref())).unwrap()
+            })
+            .unwrap_or_else(|| HeaderValue::from_static(""));
+        // this isn't the last page
+        ok(result).push_header((HeaderName::from_static("offset"), last_id))
+    })
+}
+
+/// Reset the registry
+///
+/// Reset the registry to a system default state.
+pub async fn reset_registry() -> impl IntoResponse {
+    // 200: reset registry
+    // 401: not authorized
+    StatusCode::NOT_IMPLEMENTED
+}
+
+/// Interact with the package with this ID
+///
+/// Return this package.
+pub async fn get_package_by_id(Path(_id): Path<PackageId>) -> impl IntoResponse {
+    // 200: return package
+    // 404: does not exist
+    StatusCode::NOT_IMPLEMENTED
+}
+
+/// Update the content of the package.
+///
+/// The name, version, and ID must match.
+/// The package contents (from PackageData) will replace the previous contents.
+pub async fn update_package_by_id(
+    Path(_id): Path<PackageId>,
+    Json(_info): Json<Package>,
+) -> impl IntoResponse {
+    // 200: package updated
+    // 404: does not exist
+    StatusCode::NOT_IMPLEMENTED
+}
+
+/// Delete this version of the package.
+pub async fn delete_package_by_id(Path(_id): Path<PackageId>) -> impl IntoResponse {
+    // 200: package deleted
+    // 404: does not exist
+    StatusCode::NOT_IMPLEMENTED
+}
+
+pub async fn post_package(
+    Json(Package { mut metadata, data }): Json<Package>,
+) -> Result<MyResponse<Package>, StatusCode> {
+    // not yet implemeted:
+    // 403: auth failed
+    // 424: failed due to bad rating
+    let db = constants::get_database().await;
+
+    let prev_versions_count = db
+        .fluent()
+        .select()
+        .from(constants::METADATA)
+        .filter(|q| {
+            q.for_all([
+                q.field("Name").eq(&metadata.name),
+                q.field("Version").eq(&metadata.version),
+            ])
+        })
+        .limit(1)
+        .query()
+        .await
+        .map_err(|e| {
+            log::error!("{}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?
+        .len();
+
+    if prev_versions_count >= 1 {
+        // 409: package already exists
+        log::info!("Failing package upload due to at least one matching package");
+        return Err(StatusCode::CONFLICT);
+    }
+
+    metadata.id = PackageId::new();
+
+    db.fluent()
+        .insert()
+        .into(constants::METADATA)
+        .document_id(&metadata.id)
+        .object(&metadata)
+        .execute()
+        .await
+        .map_err(|e| {
+            log::error!("{}", e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+    log::info!(
+        "Successfully uploaded new package with id {:?}",
+        metadata.id
+    );
+    // 201: return package, with correct ID
+    Ok(ok(Package { metadata, data }))
+}
+
+pub async fn get_rating_by_id(Path(_id): Path<PackageId>) -> impl IntoResponse {
+    // 200: return rating iff all rated
+    // 404: does not exist
+    // 500: package rating error
+    StatusCode::NOT_IMPLEMENTED
+}
+
+/// Create an access token.
+pub async fn authenticate(
+    Json(auth): Json<AuthenticationRequest>,
+) -> MyResponse<AuthenticationToken> {
+    // 200: return token
+    // 401: invalid user/password
+    // 501: not implemented
+    respond(StatusCode::NOT_IMPLEMENTED, AuthenticationToken::new(auth))
+}
+
+/// Return the history of this package (all versions).
+pub async fn get_package_by_name(Path(_name): Path<String>) -> impl IntoResponse {
+    // 200: return package history
+    // 404: does not exist
+    StatusCode::NOT_IMPLEMENTED
+}
+
+/// Delete all versions of this package.
+pub async fn delete_package_by_name(Path(_name): Path<String>) -> impl IntoResponse {
+    // 200: package deleted
+    // 404: does not exist
+    StatusCode::NOT_IMPLEMENTED
+}
+
+/// Get any packages fitting the regular expression.
+///
+/// Search for a package using regular expression over package names and READMEs.
+pub async fn get_package_by_regex(_regex: String) -> impl IntoResponse {
+    // 200: return list of packages
+    // 404: no packages found
+    StatusCode::NOT_IMPLEMENTED
+}
